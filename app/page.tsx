c64c2bcaf843ce31aa5a55d28d596b8c93e11ab1
Elena Kroupkin <pocoat@gmail.com>
2024-12-11 18:31:53 +1300

    Show More functionality with updated URL

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,10 @@ export default async function Home({searchParams}:HomeProps) {
   ) )}
 </div>
 
-<ShowMore/> 
+<ShowMore
+pageNumber = {(searchParams.limit || 10)/10}
+isNext = {(searchParams.limit || 10) > allCars.length}
+/> 
 
 
   </section>
--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+"use client";
 
-const ShowMore = () => {
+import React from 'react'
+import { useRouter } from 'next/navigation';
+import { ShowMoreProps } from '@/types';
+import CustomButton from './CustomButton';
+import { updateSearchParams } from '@/utils';
+
+
+
+const ShowMore = ({pageNumber, isNext}:ShowMoreProps) => {
+    const router = useRouter();
+    const handleNavigation=()=>{
+        const newLimit = (pageNumber + 1) * 10;
+        const newPathName = updateSearchParams("limit", `${newLimit}`);
+        router.push(newPathName, {scroll:false});
+    };
+
   return (
-    <div>ShowMore</div>
+    <div className='w-full flex-center gap-5 mt-10'>
+        {!isNext && <CustomButton
+        title = "Show More"
+        btnType = "button"
+        containerStyles='bg-blue-500 rounded-full text-white'
+        handleClick={handleNavigation}
+        />}
+        </div>
   )
 }
 
